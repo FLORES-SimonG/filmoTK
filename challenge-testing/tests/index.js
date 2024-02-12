341a3108cf1c7e51861c6ddbd60c0346fcea80eb
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-02-12 14:34:57 +0100

    Testing
    Comenzando a implementar JEST.

---
+++ challenge-testing/tests/index.js
@@ -0,0 +1,73 @@
+class CarritoCompra {
+  constructor() {
+    this.productos = [];
+  }
+  getAllProductos() {
+    return this.productos;
+  }
+
+  agregarProducto(producto) {
+    const articulo = new Articulo(producto);
+    this.productos.push(articulo);
+  }
+
+  calcularTotal() {
+    let valorInicialAcumulador = 0;
+    
+    for (let i=0; i<this.productos.length;i++){
+        valorInicialAcumulador+=this.productos[i].precio;
+    }
+    let valorSinDescuento=valorInicialAcumulador;
+    return valorSinDescuento;
+  }
+
+  aplicarDescuento(porcentaje=0) {
+    let valorConDescuento=0;
+    valorConDescuento= this.calcularTotal()*(1-(Math.abs(porcentaje)/100));   
+    return valorConDescuento ;
+  }
+}
+
+
+
+class Articulo {
+  constructor({ title, precio, descuento}) {
+    
+    this.title = title;
+    this.precio = precio;
+    this.descuento = descuento;
+  }
+}
+
+const carritoDeCompras = new CarritoCompra;
+
+// let prueba = {
+//     title: 'perro',
+//     precio: 100,
+//     descuento: 10
+// }
+// let prueba2 = {
+//     title: 'gato',
+//     precio: 1000,
+//     descuento: 10
+// }
+// let prueba3 = {
+//     title: 'zebra',
+//     precio: 500,
+//     descuento: 10
+// }
+// let prueba4 = {
+//     title: 'lagarto',
+//     precio: 400,
+//     descuento: 10
+// }
+
+// carritoDeCompras.agregarProducto(prueba);
+// carritoDeCompras.agregarProducto(prueba2);
+// carritoDeCompras.agregarProducto(prueba3);
+// carritoDeCompras.agregarProducto(prueba4);
+
+// console.log(carritoDeCompras.getAllProductos());
+
+// console.log(carritoDeCompras.calcularTotal())
+// console.log(carritoDeCompras.aplicarDescuento())
---
+++ challenge-testing/tests/prueba.test.js
@@ -0,0 +1,5 @@
+describe('una prueba de test que pasan siempre',()=>{
+    it('este test debe pasar siempre', ()=>{
+        expect(true).toBe(true);
+    });
+});
