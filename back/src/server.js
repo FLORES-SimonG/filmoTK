134cfae5a0d71d54814e21b9740219bf809d0fb8
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-02-15 20:31:15 +0100

    Comienzo de Express II
    Instalé Cors, llamé los Midd disponibles y he hecho uno propio para verificar su funcionamiento.

--- back/src/server.js
+++ back/src/server.js
@@ -1,8 +1,26 @@
 const express =require('express');
 const movieRouter = require('./routes/movieRouter');
 const homeRouter = require('./routes/homeRouter');
+const morgan =require('morgan');
+const cors= require('cors');
+
 const app = express();
 
+//!MIDDLEWARE -------
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+//!------------------
+
+//? SI QUIERO HACER UNO PROPIO MIDDLEWARE:
+
+app.use((req,res,next)=>{
+    console.log('Este es mi propio Middleware');
+    next();
+})
+
+
+//!derivaciones
 app.use('/',homeRouter);
 app.use('/movies',movieRouter);
 
